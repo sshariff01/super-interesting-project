665b5bd0ecc7d19f36a2475dfaa8cf44a38c9e85
shoabe.shariff <shoabe.shariff@gmail.com>
2017-08-19 03:21:17 -0400

    Created spec skeleton for get interestings of today

---
+++ super-interesting/spec/get_interestings_of_today_spec.rb
@@ -0,0 +1,12 @@
+describe "get interestings of today" do
+
+  it "should get a list of today's standup messages" do
+  end
+
+  it "should get the contents of each email" do
+  end
+
+  it "should convert the message body of each email into a human readable format" do
+  end
+
+end
