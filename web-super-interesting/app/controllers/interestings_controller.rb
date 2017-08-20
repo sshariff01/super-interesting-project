63c5c352f9e34e52e1e018ba487ffba715fa21c0
shoabe.shariff <shoabe.shariff@gmail.com>
2017-08-20 02:58:20 -0400

    Provide StandupJob with a time range

--- web-super-interesting/app/controllers/interestings_controller.rb
+++ web-super-interesting/app/controllers/interestings_controller.rb
@@ -4,7 +4,7 @@ class InterestingsController < ApplicationController
 
   def index
     standup_job = StandupJob.new
-    messages = standup_job.run
+    messages = standup_job.run(before: '2017/08/19', after: '2017/08/18')
     @all_interestings = messages.map { |msg| "#{msg['body']}" }.join("<br />")
   end
 
