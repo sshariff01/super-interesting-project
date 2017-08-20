86c7c1c6351f6ae406db4c588f20eb926874066a
shoabe.shariff <shoabe.shariff@gmail.com>
2017-08-20 03:41:33 -0400

    Display location of each message

--- web-super-interesting/app/controllers/interestings_controller.rb
+++ web-super-interesting/app/controllers/interestings_controller.rb
@@ -5,7 +5,9 @@ class InterestingsController < ApplicationController
   def index
     standup_job = StandupJob.new
     messages = standup_job.run(before: '2017/08/19', after: '2017/08/18')
-    @all_interestings = messages.map { |msg| "#{msg['body']}" }.join("<br />")
+    @all_interestings = messages.map do
+      |msg| "<h2>Interestings from #{msg['location']}</h2>#{msg['body']}"
+    end.join("<br />")
   end
 
 end
