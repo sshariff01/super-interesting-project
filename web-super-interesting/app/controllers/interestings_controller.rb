d148e87074d2b3677488971d743b0275f3aa0cde
shoabe.shariff <shoabe.shariff@gmail.com>
2017-08-21 02:21:12 -0400

    Update InterestingsController to inject dependencies into StandupJob
    - Create StandupJob with MessageDownloader, MessageDecoder, and CategoryParser

--- web-super-interesting/app/controllers/interestings_controller.rb
+++ web-super-interesting/app/controllers/interestings_controller.rb
@@ -3,11 +3,27 @@ require 'super-interesting'
 class InterestingsController < ApplicationController
 
   def index
-    standup_job = StandupJob.new
+    standup_job = StandupJob.new(
+      message_downloader: message_downloader,
+      message_decoder: message_decoder,
+      category_parser: category_parser
+    )
     messages = standup_job.run(before: '2017/08/19', after: '2017/08/18')
     @all_interestings = messages.map do
       |msg| "<h2>Interestings from #{msg['location']}</h2>#{msg['body']}"
     end.join("<br />")
+  end
+
+  def message_downloader
+    MessageDownloader.new
+  end
+
+  def message_decoder
+    MessageDecoder.new
+  end
+
+  def category_parser
+    CategoryParser.new
   end
 
 end
